200e60c65e5b132e8d77aa17e948d94518acf02f
NarendraUnnava <89415190+NarendraUnnava@users.noreply.github.com>
2021-11-30 21:32:06 -0600

    jh

--- models/car.js
+++ models/car.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const carSchema = mongoose.Schema({
     manufacturer: String,
-    model: String,
-    cost: Number
+    model: {type:String,length:15},
+    cost: {type:Number,min:1000,max:100000}
 })
 module.exports = mongoose.model("Car",
     carSchema)
